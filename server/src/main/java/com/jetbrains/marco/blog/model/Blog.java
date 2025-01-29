73ed35b30713714558560a9e2704e61c5ad92ad5
Aarya Chopkar <aaryachopkar@gmail.com>
2025-01-29 21:28:49 +0530

    feat: Blog model

---
+++ server/src/main/java/com/jetbrains/marco/blog/model/Blog.java
@@ -0,0 +1,50 @@
+package com.jetbrains.marco.blog.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import jakarta.validation.constraints.NotEmpty;
+
+@Table("blogs")
+public class Blog {
+
+    @Id
+    private Integer id;
+
+    @NotEmpty
+    private String title;
+    private String content;
+
+    public Blog() {
+    }
+
+    public Blog(int id, String title, String content) {
+        this.id = id;
+        this.content = content;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
